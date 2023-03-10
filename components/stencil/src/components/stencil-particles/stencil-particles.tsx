748ef212bd85c11be938dee662a9127ec0e981f4
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-03-10 14:29:31 +0100

    build: working on stencil component

--- components/stencil/src/components/stencil-particles/stencil-particles.tsx
+++ components/stencil/src/components/stencil-particles/stencil-particles.tsx
@@ -1,4 +1,5 @@
 import { Component, Prop, h } from '@stencil/core';
+import { Container, Engine, ISourceOptions, tsParticles } from 'tsparticles-engine';
 
 @Component({
   tag: 'stencil-particles',
@@ -10,7 +11,55 @@ export class StencilParticles {
    */
   @Prop() id: string;
 
+  @Prop() options: ISourceOptions;
+
+  @Prop() url: string;
+
+  @Prop() particlesInit: (engine: Engine) => Promise<void>;
+
+  @Prop() particlesLoaded: (container: Container) => Promise<void>;
+
+  container?: Container;
+
+  connectedCallback() {
+    const cb = () => {
+      const id = this.id ?? 'tsparticles',
+        promise = this.url ? tsParticles.loadJSON(id, this.url) : tsParticles.load(id, this.options);
+
+      promise.then(container => {
+        this.container = container;
+        this.particlesLoaded?.(container);
+      });
+    };
+
+    if (this.particlesInit) {
+      this.particlesInit(tsParticles).then(cb);
+    } else {
+      cb();
+    }
+  }
+
+  componentDidUpdate() {
+    if (!this.container) {
+      return;
+    }
+
+    this.container.destroy();
+
+    const id = this.id ?? 'tsparticles',
+      promise = this.url ? tsParticles.loadJSON(id, this.url) : tsParticles.load(id, this.options);
+
+    promise.then(container => {
+      this.container = container;
+      this.particlesLoaded?.(container);
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div id={this.id}>
+        <canvas></canvas>
+      </div>
+    );
   }
 }
